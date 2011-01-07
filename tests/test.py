862b41550c4d81c7717cd509b33f1ab8ee0f4bdb
Peter van Dijk <peter@openpanel.com>
2011-01-07 17:41:42 +0100

    use python logging for testing output

--- tests/test.py
+++ tests/test.py
@@ -8,11 +8,11 @@ def postmapq(mapname, key):
         '/etc/postfix/openpanel/'+mapname
         ], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
-def smtpauthcheck(box, password):
+def smtpauthcheck(ctx, box, password):
     s = smtplib.SMTP()
     s.connect()
     try:
-        print s.login(box,password)
+        ctx.logger.debug(s.login(box,password))
     except smtplib.SMTPAuthenticationError:
         s.quit()
         return False
@@ -24,43 +24,43 @@ def test(ctx):
     try:
         ctx.conn.rpc.classinfo(classid='Mail')
     except CoreException:
-        print 'Mail class not found, skipping tests'
+        ctx.fail("class-not-found", 'Mail class not found, skipping tests')
         return
 
     if postmapq('virtual_mailbox_domains', ctx.domain)[0]:
-        print 'Mail domain already present, ERROR'
+        ctx.fail("maildomain-already-in-postmap", 'Mail domain already present')
         return False
         
     mailuuid = ctx.conn.rpc.create(classid="Mail", objectid=ctx.domain, parentid=ctx.domainuuid)['body']['data']['objid']
-    print 'created Mail %s (%s)' % (ctx.domain, mailuuid)
+    ctx.logger.debug('created Mail %s (%s)' % (ctx.domain, mailuuid))
 
     if postmapq('virtual_mailbox_domains', ctx.domain)[0] == 'VIRTUAL\n':
-        print 'Mail %s spotted in postfix config' % (ctx.domain)
+        ctx.logger.debug('Mail %s spotted in postfix config' % (ctx.domain))
     else:
-        print 'Mail %s missing in postfix config' % (ctx.domain)
+        ctx.fail("maildomain-not-created", 'Mail %s missing in postfix config' % (ctx.domain))
         return False
         
     box=ctx.username+'@'+ctx.domain
     alias='a_'+ctx.username+'+alias@'+ctx.domain
     
-    if smtpauthcheck(box, ctx.password):
-        print 'smtp auth works before creation of box, ERROR'
+    if smtpauthcheck(ctx, box, ctx.password):
+        ctx.fail("smtp-auth-false-positive", 'smtp auth works before creation of box')
         return False
 
     boxuuid = ctx.conn.rpc.create(classid="Mail:Box", objectid=box, parentid=mailuuid, data=dict(
         password=ctx.password,
         allowrelay=True))['body']['data']['objid']
-    print 'created Mail:Box %s (%s)' % (box, boxuuid)
+    ctx.logger.debug('created Mail:Box %s (%s)' % (box, boxuuid))
 
-    if not smtpauthcheck(box, ctx.password):
-        print 'smtp auth does not work, ERROR'
+    if not smtpauthcheck(ctx, box, ctx.password):
+        ctx.fail("smtp-auth", 'smtp auth does not work')
         return False
 
     aliasuuid = ctx.conn.rpc.create(classid="Mail:Alias", objectid=alias, parentid=mailuuid, data=dict(pridest='test@example.com'))['body']['data']['objid']
     ctx.conn.rpc.delete(classid="Mail:Alias", objectid=aliasuuid)
 
     if postmapq('virtual_alias', alias)[1]:
-        print 'create+delete of alias leaves entry behind, ERROR'
+        ctx.fail("not-deleted", 'create+delete of alias leaves entry behind')
         return False
     
     return True
